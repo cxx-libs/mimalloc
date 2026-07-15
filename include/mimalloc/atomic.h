dde8134da4b40612972c0c4a10d3b73d682deb60
flagarde <flagarde.programs@pm.me>
2026-07-16 04:25:13 +0800

    use atomics

--- include/mimalloc/atomic.h
+++ include/mimalloc/atomic.h
@@ -27,9 +27,6 @@ terms of the MIT license. A copy of the license can be found in the file
 // To gain better insight in the range of used atomics, we use explicitly named memory order operations
 // instead of passing the memory order as a parameter.
 // -----------------------------------------------------------------------------------------------
-
-#if defined(__cplusplus)
-// Use C++ atomics
 #include <atomic>
 #define  _Atomic(tp)              std::atomic<tp>
 #define  mi_atomic(name)          std::atomic_##name
@@ -40,25 +37,6 @@ terms of the MIT license. A copy of the license can be found in the file
  #define MI_ATOMIC_VAR_INIT(x)    x
 #else
  #define MI_ATOMIC_VAR_INIT(x)    ATOMIC_VAR_INIT(x)
-#endif
-#elif defined(_MSC_VER)
-// Use MSVC C wrapper for C11 atomics
-#define  _Atomic(tp)              tp
-#define  MI_ATOMIC_VAR_INIT(x)    x
-#define  mi_atomic(name)          mi_atomic_##name
-#define  mi_memory_order(name)    mi_memory_order_##name
-#else
-// Use C11 atomics
-#include <stdatomic.h>
-#define  mi_atomic(name)          atomic_##name
-#define  mi_memory_order(name)    memory_order_##name
-#if (__STDC_VERSION__ >= 201710L) // c17, see issue #735
- #define MI_ATOMIC_VAR_INIT(x)    x
-#elif !defined(ATOMIC_VAR_INIT)
- #define MI_ATOMIC_VAR_INIT(x)    x
-#else
- #define MI_ATOMIC_VAR_INIT(x)    ATOMIC_VAR_INIT(x)
-#endif
 #endif
 
 // Various defines for all used memory orders in mimalloc
